23a7e462ecbc474a215c4360f2c09aea7d927a19
TanaPanda <norwegian.masaki@gmail.com>
2014-05-25 20:11:33 +0900

    May 25 version

---
+++ envelop.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+z = 10
+#グラフを何本引くかを設定します
+
+def f(x, t):
+    return 2t * x - t**2
+#包絡線の元となる関数を設定します
+
+
+def subplots():
+    "Custom subplots with axes throught the origin"
+    fig, ax = plt.subplots()
+
+    # Set the axes through the origin
+    for spine in ['left', 'bottom']:
+        ax.spines[spine].set_position('zero')
+    for spine in ['right', 'top']:
+        ax.spines[spine].set_color('none')
+    
+    return (fig, ax)
+
+
+fig, ax = subplots()
+for a in range(-z/2, z/2):
+   x = np.linspace(-5, 5, 200)
+   y = f(x, t=a)
+   ax.plot(x, y, 'b-', linewidth=2)
+   ax.legend(loc='lower right')
+plt.show()
