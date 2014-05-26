e9757df4b6739c4e21f1266acd22d1101228e08d
TanaPanda <norwegian.masaki@gmail.com>
2014-05-26 16:43:15 +0900

    May 26 Version-3

--- envelop.py
+++ envelop.py
@@ -1,34 +1,46 @@
 # -*- coding: utf-8 -*-
+from mpl_toolkits.axes_grid.axislines import SubplotZero
+from matplotlib.transforms import BlendedGenericTransform
 import matplotlib.pyplot as plt
-import numpy as np
+import numpy
 
-z = 11
+
+z=11
 #グラフを何本引くかを設定します
 #y=0があるので、偶数を入力すると左右対称になりません。
 
-x = np.linspace(-5, 5, 200)
+x = numpy.linspace(-5, 5, 1000)
 #xの範囲を指定します。
 
 def f(x, t):
     return 2 * t * x - t**2
 #包絡線の元となる関数を設定します
 
-def subplots():
-    "Custom subplots with axes throught the origin"
-    fig, ax = plt.subplots()
+if 1:
+    fig = plt.figure(1)
+    ax = SubplotZero(fig, 111)
+    fig.add_subplot(ax)
 
-    # Set the axes through the origin
-    for spine in ['left', 'bottom']:
-        ax.spines[spine].set_position('zero')
-    for spine in ['right', 'top']:
-        ax.spines[spine].set_color('none')
+    ax.axhline(linewidth=1.7, color="black")
+    ax.axvline(linewidth=1.7, color="black")
 
-    return (fig, ax)
+    plt.xticks([])
+    plt.yticks([])
 
-fig, ax = subplots()
+    ax.text(0, 1.05, 'y', transform=BlendedGenericTransform(ax.transData, ax.transAxes), ha='center')
+    ax.text(1.05, 0, 'x', transform=BlendedGenericTransform(ax.transAxes, ax.transData), va='center')
 
-for a in range(-z/2 + 1, z/2 + 1):
-    y = f(x, t=a)
-    ax.plot(x, y, 'black’, linewidth=1)
+    for direction in ["xzero", "yzero"]:
+        ax.axis[direction].set_axisline_style("-|>")
+        ax.axis[direction].set_visible(True)
 
-plt.show()
+    for direction in ["left", "right", "bottom", "top"]:
+        ax.axis[direction].set_visible(False)
+
+        
+    for a in range(-z/2 + 1, z/2 + 1):
+        y = f(x, t=a)
+        ax.plot(x, y, linewidth=1, color="black")
+
+    plt.show()
+    
